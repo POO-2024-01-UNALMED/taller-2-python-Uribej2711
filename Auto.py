47427914d752ac745081e46e48bb2f3056f3ca7f
Uribej2711 <juuribelo@unal.edu.co>
2024-02-19 22:36:20 -0500

    Todo en 1 :100:
    AAA

--- Auto.py
+++ Auto.py
@@ -27,4 +27,24 @@ class Auto:
         else:
             return "Las piezas no son originales"             
         
-        
+class Motor:
+    
+    def __init__ (self, numeroCilindros, tipo, registro):
+        self.numeroCilindrs = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+    def asignarTipo (self, tipo):
+        if(tipo == "electrico" or tipo == "gasolina"):
+            self.tipo = tipo       
+class Asiento:
+    
+    def __init__ (self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+    
+    def cambiarColor(self, color):
+        if (color =="rojo" or color == "verde" or color == "amarillo" or color == "negro" or color == "blanco"):
+            self.color = color           
