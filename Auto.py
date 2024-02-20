fdb162d8745f09281acc6f72e59693c3c0a11d17
Uribej2711 <juuribelo@unal.edu.co>
2024-02-19 23:22:07 -0500

    ya este fue.py
    :alarm_clock:

--- Auto.py
+++ Auto.py
@@ -5,21 +5,23 @@ class Auto:
     def __init__ (self, modelo, precio, asientos, marca, motor, registro):
         self.modelo = modelo
         self.precio = precio
-        self.asiento = asientos
+        self.asientos = asientos
         self.marca = marca
-        self.Motor = motor
+        self.motor = motor
         self.registro = registro
-    def cantidadAsientos(self):
+        
+        
+    def cantidadAsientos (self):
         numAsientos = 0
         for asiento in self.asientos:
-            if (type(asiento) == asiento):
+            if (type(asiento) == Asiento):
                 numAsientos += 1
         return numAsientos
 
-    def verificarIntegridad(self):
+    def verificarIntegridad (self):
         if(self.registro == self.motor.registro):  
             for asiento in self.asientos:
-                if (type(asiento) == asiento):
+                if (type(asiento) == Asiento):
                     if asiento.registro != self.registro:
                         return "Las piezas no son originales"
             return "Auto original"
